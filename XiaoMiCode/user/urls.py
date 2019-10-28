098cd92c5b60c7920250b842b17ae1aed603e9fb
Yang <18535628785@163.com>
2019-10-28 19:56:23 +0800

    code2

---
+++ XiaoMiCode/user/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+
+from user.views import register
+
+app_name = 'user'
+urlpatterns = [
+    path('register', register, name='register')
+]
--- XiaoMiCode/user/views.py
+++ XiaoMiCode/user/views.py
@@ -1,6 +1,13 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
 
+
+
+def index(request):
+    return HttpResponse('ok')
+
+
 def register(request):
-    return HttpResponse
+    return render(request,'user/register.html')
